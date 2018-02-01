2611f3f79f900b456fe9eae252a77c54cfcec2ba
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-01 13:14:04 +0900

    日付グローバル変数追加

--- myapp.rb
+++ myapp.rb
@@ -12,12 +12,14 @@ get '/index' do
   @title = "MyApp"
   ACSESS_TORKEN = ENV['ACSESS_TORKEN']
   APP_SCRET = ENV['APP_SCRET']
+  FROM_TIME = '2017/01/01'
+  TO_TIME = '2018/01/01'
   class History
     include GetFBData
   end
   my_history = History.new()
-  @from_time = Time.parse('2017/01/01')
-  @to_time = Time.parse('2018/01/01')
+  @from_time = Time.parse(FROM_TIME)
+  @to_time = Time.parse(TO_TIME)
   data_indexs = my_history.getData()
   @range_indexs = my_history.filter(data_indexs,@from_time,@to_time)
   erb :index
