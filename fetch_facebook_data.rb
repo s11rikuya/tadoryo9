3e9934888dcb372e73104dcb373bd7967ec872ac
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-01 14:12:01 +0900

    initialize追加 構造変更

--- fetch_facebook_data.rb
+++ fetch_facebook_data.rb
@@ -1,22 +1,22 @@
 module GetFBData
   def getData()
-    @graph = Koala::Facebook::API.new(ACSESS_TORKEN)
+    @graph = Koala::Facebook::API.new(@acsess_torken)
     @graph_result = @graph.get_connection('me', 'posts', { fields: %w(place created_time) } )
     @results = @graph_result.to_a
     until (next_results = @graph_result.next_page).nil?
       @results += next_results.to_a
       @graph_result = next_results
     end
-    @graph = Koala::Facebook::API.new(ACSESS_TORKEN, APP_SCRET)
+    @graph = Koala::Facebook::API.new(@acsess_torken, @app_secret)
     data_indexs = @results
     return data_indexs
   end
 
-  def filter(data_indexs,from_time,to_time)
+  def filter(data_indexs)
     range_indexs = []
     data_indexs.each do |index|
       next unless index['place'] && index['created_time']
-      next unless from_time <= Time.parse(index['created_time']) && Time.parse(index['created_time']) <= to_time
+      next unless @from_time <= Time.parse(index['created_time']) && Time.parse(index['created_time']) <= @to_time
       container = {}
       container['time'] = index['created_time']
       if index['place']['name'] == ""
