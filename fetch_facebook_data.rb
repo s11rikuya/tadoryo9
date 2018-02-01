bc7d8e71b3a2979b20e14e9801c409b9a327ecba
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-01 21:39:30 +0900

    rubocop修正 map reject利用

--- fetch_facebook_data.rb
+++ fetch_facebook_data.rb
@@ -1,34 +1,31 @@
 module GetFBData
   require 'koala'
+  require 'pp'
   def getData()
-    @graph = Koala::Facebook::API.new(@accsess_torken)
-    @graph_result = @graph.get_connection('me', 'posts', { fields: %w(place created_time) } )
-    @results = @graph_result.to_a
-    until (next_results = @graph_result.next_page).nil?
-      @results += next_results.to_a
-      @graph_result = next_results
+    graph = Koala::Facebook::API.new(@accsess_torken)
+    graph_result = graph.get_connection('me', 'posts', { fields: %w(place created_time) } )
+    results = graph_result.to_a
+    until (next_results = graph_result.next_page).nil?
+      results += next_results.to_a
+      graph_result = next_results
     end
-    @graph = Koala::Facebook::API.new(@accsess_torken, @app_secret)
-    data_indexs = @results
-    return data_indexs
+    graph = Koala::Facebook::API.new(@accsess_torken, @app_secret)
+    data_indexs = results
   end
 
   def filter(data_indexs)
-    range_indexs = []
-    data_indexs.each do |index|
-      next unless index['place'] && index['created_time']
-      next unless @from_time <= Time.parse(index['created_time']) && Time.parse(index['created_time']) <= @to_time
-      container = {}
-      container['time'] = index['created_time']
-      if index['place']['name'] == ""
-        container['name'] = "NO NAME"
-      else
-        container['name'] = index['place']['name']
-      end
-      container['lat'] = index['place']['location']['latitude']
-      container['lng'] = index['place']['location']['longitude']
-      range_indexs.push(container)
+    data_indexs.reject do |di|
+      di['place'].nil?
+    end.select do |di|
+      Time.parse(di['created_time']).between?(@from_time, @to_time)
+    end.map do |di|
+      di['place']['name'] = 'NO NAME' if di['place']['name'] == ''
+      {
+        time: di['created_time'],
+        name: di['place']['name'],
+        lat: di['place']['location']['latitude'],
+        lng: di['place']['location']['longitude']
+      }
     end
-    return range_indexs
   end
 end
--- myapp.rb
+++ myapp.rb
@@ -8,15 +8,15 @@ Dotenv.load
 
 ACCSESS_TORKEN = ENV['ACCSESS_TORKEN']
 APP_SCRET = ENV['APP_SCRET']
-FROM_TIME = '2017/01/01'
-TO_TIME = '2018/01/01'
+FROM_TIME = '2017/01/01'.freeze
+TO_TIME = '2017/09/01'.freeze
 
 get '/index' do
   require_relative 'fetch_facebook_data.rb'
-  @title = "MyApp"
+  @title = 'Tadoryo9'
   class History
     attr_accessor :from_time, :to_time
-    def initialize(accesss_torken,app_secret,from_time,to_time)
+    def initialize(accesss_torken, app_secret, from_time, to_time)
       @accsess_torken = accesss_torken
       @app_secret = app_secret
       @from_time = Time.parse(from_time)
@@ -24,8 +24,8 @@ get '/index' do
     end
     include GetFBData
   end
-  my_history = History.new(ACCSESS_TORKEN,APP_SCRET,FROM_TIME,TO_TIME)
-  data_indexs = my_history.getData()
+  my_history = History.new(ACCSESS_TORKEN, APP_SCRET, FROM_TIME, TO_TIME)
+  data_indexs = my_history.getData
   @from_time = my_history.from_time
   @to_time = my_history.to_time
   @range_indexs = my_history.filter(data_indexs)
