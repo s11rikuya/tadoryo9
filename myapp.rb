2146f4d528f322556352cdfd45afe1054f458c85
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-01 14:12:01 +0900

    initialize追加 構造変更

--- myapp.rb
+++ myapp.rb
@@ -7,20 +7,28 @@ require 'time'
 require 'dotenv'
 Dotenv.load
 
+ACSESS_TORKEN = ENV['ACSESS_TORKEN']
+APP_SCRET = ENV['APP_SCRET']
+FROM_TIME = '2017/01/01'
+TO_TIME = '2018/01/01'
+
 get '/index' do
   require_relative 'fetch_facebook_data.rb'
   @title = "MyApp"
-  ACSESS_TORKEN = ENV['ACSESS_TORKEN']
-  APP_SCRET = ENV['APP_SCRET']
-  FROM_TIME = '2017/01/01'
-  TO_TIME = '2018/01/01'
   class History
+    attr_accessor :from_time, :to_time
+    def initialize(acsess_torken,app_secret,from_time,to_time)
+      @acsess_torken = acsess_torken
+      @app_secret = app_secret
+      @from_time = Time.parse(from_time)
+      @to_time = Time.parse(to_time)
+    end
     include GetFBData
   end
-  my_history = History.new()
-  @from_time = Time.parse(FROM_TIME)
-  @to_time = Time.parse(TO_TIME)
+  my_history = History.new(ACSESS_TORKEN,APP_SCRET,FROM_TIME,TO_TIME)
   data_indexs = my_history.getData()
-  @range_indexs = my_history.filter(data_indexs,@from_time,@to_time)
+  @from_time = my_history.from_time
+  @to_time = my_history.to_time
+  @range_indexs = my_history.filter(data_indexs)
   erb :index
 end
