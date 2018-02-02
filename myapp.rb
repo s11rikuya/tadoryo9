21ea449557c9fd54d0ea831979d05d7a9ebb09cd
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-02 16:37:45 +0900

    指摘修正

--- myapp.rb
+++ myapp.rb
@@ -6,25 +6,25 @@ require 'time'
 require 'dotenv'
 Dotenv.load
 
-ACCSESS_TORKEN = ENV['ACCSESS_TORKEN']
+ACCESS_TOKEN = ENV['ACCESS_TOKEN']
 APP_SCRET = ENV['APP_SCRET']
-FROM_TIME = '2017/01/01'.freeze
-TO_TIME = '2017/09/01'.freeze
+FROM_TIME = '2016/01/01'.freeze
+TO_TIME = '2016/10/01'.freeze
 
 get '/index' do
   require_relative 'fetch_facebook_data.rb'
   @title = 'Tadoryo9'
   class History
     attr_accessor :from_time, :to_time
-    def initialize(accesss_torken, app_secret, from_time, to_time)
-      @accsess_torken = accesss_torken
+    def initialize(access_token, app_secret, from_time, to_time)
+      @access_token = access_token
       @app_secret = app_secret
       @from_time = Time.parse(from_time)
       @to_time = Time.parse(to_time)
     end
     include GetFBData
   end
-  my_history = History.new(ACCSESS_TORKEN, APP_SCRET, FROM_TIME, TO_TIME)
+  my_history = History.new(ACCESS_TOKEN, APP_SCRET, FROM_TIME, TO_TIME)
   data_indexs = my_history.gets_data
   @from_time = my_history.from_time
   @to_time = my_history.to_time
