3ce71518d6e5527c15977b75056be0cc10b0af43
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-22 14:51:09 +0900

    require場所変更 app_secret削除

--- history.rb
+++ history.rb
@@ -1,11 +1,11 @@
+require_relative 'fetch_facebook_data.rb'
+require_relative 'point_calculation.rb'
+
 class History
-  require_relative 'fetch_facebook_data.rb'
-  require_relative 'point_calculation.rb'
   include GetFBData
   include PointCalculation
   attr_accessor :since_time, :until_time
-  def initialize(access_token, app_secret)
+  def initialize(access_token)
     @access_token = access_token
-    @app_secret = app_secret
   end
 end
